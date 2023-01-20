1482455f3122e72701cb0e0369a865fc125d5234
TerrificTable <omegagaming189@gmail.com>
2023-01-21 00:49:09 +0100

    the main webserver

--- webserver/webserver.go
+++ webserver/webserver.go
@@ -1,14 +1,32 @@
 package webserver
 
 import (
+	"fmt"
 	"main/webserver/handlers"
+	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
-func NewWebServer(port int) {
+type WebServer struct {
+	Router *mux.Router
+}
+
+func NewWebServer() *WebServer {
 	r := mux.NewRouter()
 
 	r.HandleFunc("/", handlers.IndexHandler)
+	r.HandleFunc("/search", handlers.SearchHandler)
 
+	return &WebServer{
+		Router: r,
+	}
+}
+
+func (ws *WebServer) Start(port int, handler http.Handler) error {
+	http.Handle("/", ws.Router)
+
+	fmt.Printf("Starting webserver: http://localhost:%d\n", port)
+
+	return http.ListenAndServe(fmt.Sprintf(":%d", port), handler)
 }
