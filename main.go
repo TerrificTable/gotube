bc7d88ca33888b45e748a939db3b99e10b44f5b5
TerrificTable <omegagaming189@gmail.com>
2023-01-21 00:48:28 +0100

    only start webserver which handles everything else

--- main.go
+++ main.go
@@ -2,47 +2,17 @@ package main
 
 import (
 	"flag"
+	"main/util"
 	"main/webserver"
+	"main/data"
 )
 
-var (
-	query      = flag.String("query", "Google", "Search term")
-	maxResults = flag.Int64("max-results", 25, "Max YouTube results")
-)
-
-const developerKey = "AIzaSyCCA-ZUq_XZJT-FBoimui783TiST0Zw7Ns"
+const developerKey = ""
 
 func main() {
 	flag.Parse()
 
-	// service := util.NewService(developerKey)
-
-	// // Make the API call to YouTube.
-	// response, err := service.Search(*query, *maxResults)
-	// if err != nil {
-	// 	panic(err)
-	// }
-
-	// // Group video, channel, and playlist results in separate lists.
-	// videos := make(map[string]string)
-	// channels := make(map[string]string)
-	// playlists := make(map[string]string)
-
-	// // Iterate through each item and add it to the correct list.
-	// for _, item := range response.Items {
-	// 	switch item.Id.Kind {
-	// 	case "youtube#video":
-	// 		videos[item.Id.VideoId] = item.Snippet.Title
-	// 	case "youtube#channel":
-	// 		channels[item.Id.ChannelId] = item.Snippet.Title
-	// 	case "youtube#playlist":
-	// 		playlists[item.Id.PlaylistId] = item.Snippet.Title
-	// 	}
-	// }
-
-	// util.PrintSection("Videos", videos)
-	// util.PrintSection("Channels", channels)
-	// util.PrintSection("Playlists", playlists)
+	data.Service = util.NewService(developerKey)
 
 	ws := webserver.NewWebServer()
 	if err := ws.Start(1234, nil); err != nil {
