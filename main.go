7c44d6f62041c81ea054c40bd78584d59b12c90b
TerrificTable <omegagaming189@gmail.com>
2023-01-20 21:52:57 +0100

    moved to webserver

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"flag"
-	"main/util"
+	"main/webserver"
 )
 
 var (
@@ -15,32 +15,38 @@ const developerKey = "AIzaSyCCA-ZUq_XZJT-FBoimui783TiST0Zw7Ns"
 func main() {
 	flag.Parse()
 
-	service := util.NewService(developerKey)
+	// service := util.NewService(developerKey)
 
-	// Make the API call to YouTube.
-	response, err := service.Search(*query, *maxResults)
-	if err != nil {
+	// // Make the API call to YouTube.
+	// response, err := service.Search(*query, *maxResults)
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	// // Group video, channel, and playlist results in separate lists.
+	// videos := make(map[string]string)
+	// channels := make(map[string]string)
+	// playlists := make(map[string]string)
+
+	// // Iterate through each item and add it to the correct list.
+	// for _, item := range response.Items {
+	// 	switch item.Id.Kind {
+	// 	case "youtube#video":
+	// 		videos[item.Id.VideoId] = item.Snippet.Title
+	// 	case "youtube#channel":
+	// 		channels[item.Id.ChannelId] = item.Snippet.Title
+	// 	case "youtube#playlist":
+	// 		playlists[item.Id.PlaylistId] = item.Snippet.Title
+	// 	}
+	// }
+
+	// util.PrintSection("Videos", videos)
+	// util.PrintSection("Channels", channels)
+	// util.PrintSection("Playlists", playlists)
+
+	ws := webserver.NewWebServer()
+	if err := ws.Start(1234, nil); err != nil {
 		panic(err)
 	}
 
-	// Group video, channel, and playlist results in separate lists.
-	videos := make(map[string]string)
-	channels := make(map[string]string)
-	playlists := make(map[string]string)
-
-	// Iterate through each item and add it to the correct list.
-	for _, item := range response.Items {
-		switch item.Id.Kind {
-		case "youtube#video":
-			videos[item.Id.VideoId] = item.Snippet.Title
-		case "youtube#channel":
-			channels[item.Id.ChannelId] = item.Snippet.Title
-		case "youtube#playlist":
-			playlists[item.Id.PlaylistId] = item.Snippet.Title
-		}
-	}
-
-	util.PrintSection("Videos", videos)
-	util.PrintSection("Channels", channels)
-	util.PrintSection("Playlists", playlists)
 }
