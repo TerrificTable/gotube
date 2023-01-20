a5edd6211f05d257a37d1ff08e4165837f608d4c
TerrificTable <omegagaming189@gmail.com>
2023-01-21 00:49:17 +0100

    route handlers

--- webserver/handlers/indexHandler.go
+++ webserver/handlers/indexHandler.go
@@ -3,8 +3,23 @@ package handlers
 import (
 	"fmt"
 	"net/http"
+	"text/template"
+	"main/data"
 )
 
-func IndexHandler(resp http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(resp, "Hello World!")
+func IndexHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		apiKey := r.FormValue("api_key")
+
+		fmt.Fprintf(w, "API key: %s\n", apiKey)
+
+		// http.Redirect(w, r, "/", 301)
+		return
+	}
+
+	t := template.Must(template.ParseGlob("webserver/static/*"))
+	err := t.ExecuteTemplate(w, "index.gohtml", data.YoutubeSearchData)
+	if err != nil {
+		fmt.Fprintf(w, err.Error())
+	}
 }
--- webserver/handlers/searchHandler.go
+++ webserver/handlers/searchHandler.go
@@ -1,1 +1,34 @@
 package handlers
+
+import (
+	"net/http"
+	"main/data"
+	"strconv"
+	"fmt"
+)
+
+func SearchHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		term := r.FormValue("s")
+		maxResults, err := strconv.Atoi(r.FormValue("max"))
+		if err != nil {
+			fmt.Fprintf(w, "Failed to convert '%s' to int", r.FormValue("max"))
+			return
+		}
+
+		response, err := data.Service.Search(term, int64(maxResults))
+		if err != nil {
+			fmt.Fprintf(w, "Failed to search: %s", err.Error())
+			return
+		}
+
+		data.YoutubeSearchData = *data.NewSearchData(*response)
+
+		//fmt.Fprintf(w, "Success")
+
+		http.Redirect(w, r, "/", 200)
+
+	} else {
+		http.NotFound(w, r)
+	}
+}
